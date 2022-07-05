5c12ca57c47417b8ae2ff352ee3f1d49e488482c
koshi01 <105272583+koshi01@users.noreply.github.com>
2022-07-05 11:03:26 +0900

    f

--- shiftproject/shiftproject/settings.py
+++ shiftproject/shiftproject/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 LOGIN_REDIRECT_URL='shift:store-info'
-LOGOUT_REDIRECT_URL='index'
+LOGOUT_REDIRECT_URL='accounts:login'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
